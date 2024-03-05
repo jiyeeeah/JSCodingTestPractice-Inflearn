160d59d7936c6c99f96300974254792313360a87
jiyeeeah <sije3757@snu.ac.kr>
2024-03-05 18:32:59 +0900

    4-2

--- section4/4-2.js
+++ section4/4-2.js
@@ -1,4 +1,22 @@
 function isPrime(num) {
+  if (num === 1) return false;
+  for (let i = 2; i <= num / 2; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(arr) {
+  let answer = [];
+  for (x of arr) {
+    let res = 0;
+    while (x) {
+      let t = x % 10;
+      res = res * 10 + t;
+      x = parseInt(x / 10);
+    }
+    if (isPrime(res)) answer.push(res);
+  }
   return answer;
 }
 
