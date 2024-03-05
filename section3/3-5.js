aa651fadee250fae8f5b76d7030b325f48f50be0
jiyeeeah <sije3757@snu.ac.kr>
2024-03-05 16:56:36 +0900

    3-5

--- section3/3-5.js
+++ section3/3-5.js
@@ -1,6 +1,21 @@
 function solution(s) {
-  let answer;
-  return answer;
+  let answer = [];
+  let p = 0;
+  let letter = s[0];
+  for (let x of s) {
+    if (x === letter) {
+      p++;
+      answer.pop();
+    } else {
+      if (p != 1) {
+        answer.push(p);
+      }
+      letter = x;
+      p = 1;
+    }
+    answer.push(letter);
+  }
+  return answer.join("");
 }
 
 let str = "KKHSSSSSSSE";
