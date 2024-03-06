cbe6e44eef8eb7b82bed60c2814a74aba21d6109
jiyeeeah <sije3757@snu.ac.kr>
2024-03-06 11:00:46 +0900

    4-4 풀이

--- section4/4-4.js
+++ section4/4-4.js
@@ -1,5 +1,20 @@
 function solution(m, product) {
-  let answer;
+  let answer = 0;
+  let n = product.length;
+  product.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
+  for (let i = 0; i < n; i++) {
+    let money = m - (product[i][0] / 2 + product[i][1]);
+    let cnt = 1;
+    for (let j = 0; j < n; j++) {
+      // 쓸데없이 for문 돌지 않도록 (현재 가격이 예산보다 클 경우 break)
+      if (j !== i && product[j][0] + product[j][1] > money) break;
+      if (j !== i && product[j][0] + product[j][1] <= money) {
+        money -= product[j][0] + product[j][1];
+        cnt++;
+      }
+    }
+    answer = Math.max(answer, cnt); // 현재 answer값과 cnt중 더 큰 값만 남김
+  }
   return answer;
 }
 
