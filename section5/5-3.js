f48c93643d189e64f37623890c076d8c621a767e
jiyeeeah <sije3757@snu.ac.kr>
2024-03-06 13:25:00 +0900

    5-3 풀이

--- section5/5-3.js
+++ section5/5-3.js
@@ -1,5 +1,17 @@
 function solution(m, arr) {
-  let answer;
+  let answer = 0;
+  let sum = 0;
+  let lt = (rt = 0);
+  while (rt < arr.length) {
+    if (sum === m) {
+      answer++;
+      sum -= arr[lt++];
+    } else if (sum < m) {
+      sum += arr[rt++];
+    } else if (sum > m) {
+      sum -= arr[lt++];
+    }
+  }
   return answer;
 }
 
