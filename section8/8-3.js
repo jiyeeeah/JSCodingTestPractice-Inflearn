3c1d8f1422f54575a7ae148b9bfe3f30016156a1
jiyeeeah <sije3757@snu.ac.kr>
2024-03-12 18:00:59 +0900

    8-3 풀이

---
+++ section8/8-3.js
@@ -0,0 +1,17 @@
+function solution(n) {
+  let answer = "";
+  function DFS(v) {
+    if (v > 7) {
+      return;
+    } else {
+      DFS(v * 2);
+      DFS(v * 2 + 1);
+      console.log(v);
+    }
+  }
+
+  DFS(n);
+  return answer;
+}
+
+console.log(solution(1));
