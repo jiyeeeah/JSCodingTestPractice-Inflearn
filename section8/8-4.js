440ce13fffea79be687ecf2810afa03320fc6456
jiyeeeah <sije3757@snu.ac.kr>
2024-03-13 10:16:11 +0900

    8-4 선생님 풀이

--- section8/8-4.js
+++ section8/8-4.js
@@ -1,12 +1,18 @@
 function solution(n) {
   let answer = [];
+  // 포함시켰는데 안시켰는지 체크하는 배열 (1부터 n까지 길이는 n+1)
+  let ch = Array.from({ length: n + 1 }, () => 0);
   function DFS(v) {
     if (v > n) {
-      console.log(answer);
+      let tmp = "";
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === 1) tmp += i + " "; // 체크배열에 체크되어 있는 애들의 인덱스만 넣어줌
+      }
+      if (tmp.length > 0) answer.push(tmp.trim());
     } else {
-      answer.push(v);
+      ch[v] = 1; // 들어갔다고 체크
       DFS(v + 1);
-      answer.pop();
+      ch[v] = 0; // 안들어갔다고 체크
       DFS(v + 1);
     }
   }
