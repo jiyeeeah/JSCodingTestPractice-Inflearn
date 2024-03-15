fc0607d38773a90804e63f35e21fa791c29e0aff
jiyeeeah <sije3757@snu.ac.kr>
2024-03-15 11:21:55 +0900

    8-11 풀이

--- section8/8-11.js
+++ section8/8-11.js
@@ -1,5 +1,13 @@
 function solution(n) {
-  let answer;
+  let answer = 1;
+  function DFS(L, fac) {
+    if (L <= 0) {
+      answer = fac;
+    } else {
+      DFS(L - 1, fac * L);
+    }
+  }
+  DFS(n, 1);
   return answer;
 }
 
