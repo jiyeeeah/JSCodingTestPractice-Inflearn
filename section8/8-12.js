223c12fcab299263492d0c7236c6e99f82b7a3a2
jiyeeeah <sije3757@snu.ac.kr>
2024-03-15 12:54:24 +0900

    8-12 메모이제이션

--- section8/8-12.js
+++ section8/8-12.js
@@ -1,8 +1,10 @@
 function solution(n, r) {
   let answer;
+  let dy = Array.from(Array(35), () => Array(35).fill(0));
   function DFS(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
     if (n - r === 0 || r === 0) return 1;
-    else return DFS(n - 1, r - 1) + DFS(n - 1, r);
+    else return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
   }
   answer = DFS(n, r);
   return answer;
