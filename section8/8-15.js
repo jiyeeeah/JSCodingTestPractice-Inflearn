b6c2e301a95ad51ec4d922fcddfe7b2badb836af
jiyeeeah <sije3757@snu.ac.kr>
2024-03-15 16:04:36 +0900

    8-15 풀이

--- section8/8-15.js
+++ section8/8-15.js
@@ -1,7 +1,17 @@
-function solution(n, k, arr, m) {
-  let answer;
+function solution(k, arr, m) {
+  let answer = 0;
+  function DFS(L, S, sum) {
+    if (L === k && sum % m === 0) {
+      answer++;
+    } else {
+      for (let i = S; i < arr.length; i++) {
+        DFS(L + 1, i + 1, sum + arr[i]);
+      }
+    }
+  }
+  DFS(0, 0, 0);
   return answer;
 }
 
 let arr = [2, 4, 5, 8, 12];
-console.log(solution(5, 3, arr, 6));
+console.log(solution(3, arr, 6));
