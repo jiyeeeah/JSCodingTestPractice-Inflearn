d460edf33b03cd4c925f9d60e3fd590d5d340c62
jiyeeeah <sije3757@snu.ac.kr>
2024-03-15 11:02:48 +0900

    8-10 선생님 풀이

--- section8/8-10.js
+++ section8/8-10.js
@@ -1,5 +1,22 @@
 function solution(m, arr) {
-  let answer;
+  let answer = [];
+  let tmp = Array.from({ length: m }, () => 0);
+  let ch = Array.from({ length: arr.length }, () => false);
+  function DFS(L) {
+    if (L >= m) {
+      answer.push([...tmp]);
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        if (!ch[i]) {
+          ch[i] = true;
+          tmp[L] = arr[i];
+          DFS(L + 1);
+          ch[i] = false;
+        }
+      }
+    }
+  }
+  DFS(0);
   return answer;
 }
 
