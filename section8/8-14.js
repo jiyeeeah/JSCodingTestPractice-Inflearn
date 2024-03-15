b43529451001a03b0d1a66d8a19fa68f0a36c3f5
jiyeeeah <sije3757@snu.ac.kr>
2024-03-15 14:15:48 +0900

    8-14 풀이

--- section8/8-14.js
+++ section8/8-14.js
@@ -1,5 +1,18 @@
 function solution(n, m) {
-  let answer;
+  let answer = [];
+  let tmp = Array.from({ length: m }, () => 0);
+  function DFS(L, S) {
+    if (L === m) {
+      answer.push(tmp.slice());
+    } else {
+      for (let i = S; i <= n; i++) {
+        // i 가 뽑은 숫자임
+        tmp[L] = i;
+        DFS(L + 1, i + 1);
+      }
+    }
+  }
+  DFS(0, 1);
   return answer;
 }
 
