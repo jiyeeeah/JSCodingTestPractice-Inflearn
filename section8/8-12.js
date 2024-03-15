b44bdf011a9166972eaaa71cdfe58c62843fa126
jiyeeeah <sije3757@snu.ac.kr>
2024-03-15 12:48:25 +0900

    8-12 풀이

--- section8/8-12.js
+++ section8/8-12.js
@@ -1,6 +1,12 @@
 function solution(n, r) {
   let answer;
+  function DFS(n, r) {
+    if (n - r === 0 || r === 0) return 1;
+    else return DFS(n - 1, r - 1) + DFS(n - 1, r);
+  }
+  answer = DFS(n, r);
   return answer;
 }
 
 console.log(solution(5, 3));
+console.log(solution(33, 19));
