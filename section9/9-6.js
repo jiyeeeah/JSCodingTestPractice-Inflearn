d864d57cf94294af95b6f3f3e1ee9979960b2203
jiyeeeah <sije3757@snu.ac.kr>
2024-03-18 19:53:25 +0900

    9-6 풀이

--- section9/9-6.js
+++ section9/9-6.js
@@ -1,6 +1,22 @@
 function solution(s, e) {
-  let answer;
+  let answer = 0;
+  let dis = Array.from({ length: 10001 }, () => 0);
+  let queue = [];
+  queue.push(s);
+  dis[s] = 0;
+  while (queue.length) {
+    let x = queue.shift();
+    for (let nx of [x - 1, x + 1, x + 5]) {
+      if (nx === e) return dis[x] + 1;
+      if (nx > 0 && nx <= 10000 && dis[nx] === 0) {
+        queue.push(nx);
+        dis[nx] = dis[x] + 1;
+      }
+    }
+  }
+
   return answer;
 }
 
+console.log(solution(5, 14));
 console.log(solution(8, 3));
