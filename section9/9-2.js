dc15e486d1c0afd2cd2533ea10f009efcd5d7d2a
jiyeeeah <sije3757@snu.ac.kr>
2024-03-18 14:57:55 +0900

    9-2 풀이

--- section9/9-2.js
+++ section9/9-2.js
@@ -1,5 +1,25 @@
 function solution(n, arr) {
-  let answer;
+  let answer = 0;
+  let graph = Array.from(Array(5), () => Array(5).fill(0));
+  for (let edge of arr) {
+    graph[edge[0] - 1][edge[1] - 1] = 1;
+  }
+
+  let ch = Array.from({ length: n + 1 }, () => false);
+  function DFS(N) {
+    if (N === n) {
+      answer++;
+    } else {
+      for (let i = 1; i <= n; i++) {
+        if (graph[N - 1][i - 1] !== 0 && !ch[N]) {
+          ch[N] = true;
+          DFS(i);
+          ch[N] = false;
+        }
+      }
+    }
+  }
+  DFS(1);
   return answer;
 }
 
