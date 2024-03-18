a99e7102498c914dc90691f8fbf19feaf819a668
jiyeeeah <sije3757@snu.ac.kr>
2024-03-18 16:25:54 +0900

    9-4 풀이

--- section9/9-4.js
+++ section9/9-4.js
@@ -1,5 +1,24 @@
 function solution(board) {
-  let answer;
+  let answer = 0;
+  let n = board.length;
+  let dx = [0, 0, -1, 1];
+  let dy = [-1, 1, 0, 0];
+  function DFS(x, y) {
+    if (x === n - 1 && y === n - 1) answer++;
+    else {
+      for (let i = 0; i < 4; i++) {
+        if (x + dx[i] >= 0 && x + dx[i] < n && y + dy[i] >= 0 && y + dy[i] < n) {
+          if (board[x + dx[i]][y + dy[i]] === 0) {
+            board[x + dx[i]][y + dy[i]] = -1;
+            DFS(x + dx[i], y + dy[i]);
+            board[x + dx[i]][y + dy[i]] = 0;
+          }
+        }
+      }
+    }
+  }
+  board[0][0] = -1;
+  DFS(0, 0);
   return answer;
 }
 
