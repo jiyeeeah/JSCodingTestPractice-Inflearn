15c3dc96d2502b5d09d48d7007c179074c6a6d85
jiyeeeah <sije3757@snu.ac.kr>
2024-03-25 14:09:09 +0900

    8-11 오답 다시

---
+++ section8/8-11-2try.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  let answer = 1;
+  function DFS(num) {
+    if (num === 1) return 1;
+    else {
+      return num * DFS(num - 1);
+    }
+  }
+  answer = DFS(n);
+  return answer;
+}
+
+console.log(solution(5));
