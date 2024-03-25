0c391ac771743a4eb73d1750acb3f9875bc0e9f0
jiyeeeah <sije3757@snu.ac.kr>
2024-03-25 17:27:14 +0900

    9-7 오답 다시

---
+++ section9/9-7-DFS-2try.js
@@ -0,0 +1,37 @@
+function solution(board) {
+  let answer = 0;
+  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+  function DFS(x, y) {
+    board[x][y] = 0;
+    for (let k = 0; k < 8; k++) {
+      let nx = x + dx[k];
+      let ny = y + dy[k];
+      if (nx >= 0 && nx < board.length && ny >= 0 && ny < board.length && board[nx][ny] === 1) {
+        DFS(nx, ny);
+      }
+    }
+  }
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] === 1) {
+        answer++;
+        DFS(i, j);
+      }
+    }
+  }
+  return answer;
+}
+
+let arr = [
+  [1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0],
+];
+
+console.log(solution(arr));
